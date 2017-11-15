e36002ccace14e574a5970e7603d4ff6b5e8addc
Youssef Kholeif <yykholeif@gmail.com>
2017-11-14 19:01:41 -0800

    Refactor getty images api

--- app/models/getty_images_adaptor.rb
+++ app/models/getty_images_adaptor.rb
@@ -5,7 +5,7 @@ class GettyImagesAdaptor
   base_uri 'https://api.gettyimages.com/v3/'
   @@token = ENV['GETTY_IMAGES_API_KEY']
 
-  headers        Authorization: "Bearer #{ENV['GETTY_IMAGES_API_KEY']}"
+  headers        Authorization: "Bearer #{@@token}"
 
   def self.return_image(query)
     response = self.get("/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=#{query}&")
