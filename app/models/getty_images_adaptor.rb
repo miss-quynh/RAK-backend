8e077d0f308efedbaabe015af68903c036b89793
Youssef Kholeif <yykholeif@gmail.com>
2017-11-14 18:02:47 -0800

    Getty images api

---
+++ app/models/getty_images_adaptor.rb
@@ -0,0 +1,19 @@
+class GettyImagesAdaptor
+
+  include HTTParty
+
+  base_uri 'https://api.gettyimages.com/v3/'
+  @@token = ENV['GETTY_IMAGES_API_KEY']
+
+  headers        Authorization: "Bearer #{ENV['GETTY_IMAGES_API_KEY']}"
+
+  def self.return_image(query)
+    response = self.get("/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=#{query}&")
+    return parse_data(response)
+  end
+
+  def self.parse_data(response)
+    response["images"][0]["display_sizes"]["uri"]
+  end
+
+end
