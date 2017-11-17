dd5a4bdef5d6f69211789348e503f4bd366486ac
flash00 <dexlorraine@gmail.com>
2017-11-16 21:14:59 -0800

    add search

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
 
+  get '/organizations/search' => 'organizations#search'
+
   post '/organization_token' => 'organization_token#create'
   post '/donor_token' => 'donor_token#create'
-
-  resources :organizations
-  post '/organizations/ein', to: 'organizations#ein'
-
-	resources :projects do
-		resources :events, only: [:show, :update]
-	end
-
-  resources :donors, except: [:index]
-
-  resources :donations, only: [:show, :create, :update]
 
   get '/filters', to: 'donors#filter_options'
   post '/filters', to: 'donors#filter'
 
+  resources :organizations
+  post '/organizations/ein', to: 'organizations#ein'
+
+  resources :projects do
+    resources :events, only: [:show, :update]
+  end
+
+  resources :donors, except: [:index]
+
+  resources :donations, only: [:show, :create, :update]
 
 end
